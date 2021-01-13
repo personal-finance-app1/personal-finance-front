43bb34d011646cf32f8e9be1e02ed5c9eea31632
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-13 15:38:47 -0600

    created feature module

---
+++ src/app/modules/balance-projection/balance-projection.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectionService } from './service/projection-service/projection.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [ProjectionService]
+})
+export class BalanceProjectionModule { }
