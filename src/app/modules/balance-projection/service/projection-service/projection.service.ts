935ad5fd894458d86c99e81dac66ae4878de2fa8
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-13 15:38:47 -0600

    created feature module

---
+++ src/app/modules/balance-projection/service/projection-service/projection.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { BalanceProjectionModule } from '../../balance-projection.module';
+
+@Injectable({
+  providedIn: BalanceProjectionModule
+})
+export class ProjectionService {
+
+  constructor() { }
+}
