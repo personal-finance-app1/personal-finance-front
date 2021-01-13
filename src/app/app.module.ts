9eb00dbdecf41f308d359d3b5f821ee7c93c0709
magnusjw <magnusjw@clarkson.edu>
2021-01-13 16:00:24 -0500

    init commit of declareBalance branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DeclareBalanceComponent } from './declareBalance/declare-balance/declare-balance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeclareBalanceComponent
   ],
   imports: [
     BrowserModule,
