7f3873381760734bbccc879b27669ec6bb96c585
askakhila <askakhila@yahoo.com>
2021-01-14 13:29:35 -0600

    setup the html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeclareExpensesComponent } from './modules/declare-expenses/component/declare-expenses/declare-expenses.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "expenses",
+  component: DeclareExpensesComponent
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
