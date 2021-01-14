936fd0a2d304267d147ea28a26c8743917fdfce7
Berin <berin2@pdx.edu>
2021-01-14 12:34:01 -0800

    Added stub functionality for services and component.
    I created the interface for our local balance service, and I created the interface for our balance component. There is no implementation as we are doing TDD. This should enable our other team members to begin work on their assigned tasks.

--- src/app/modules/balance/service/balance.service.ts
+++ src/app/modules/balance/service/balance.service.ts
@@ -6,4 +6,29 @@ import { Injectable } from '@angular/core';
 export class BalanceService {
 
   constructor() { }
+
+  /**The validate account balance is used to ensure the user has entered a valid declaring or update balance.
+  * @returns boolean which indicates if the balance is valid or invalid.
+  */
+  public validateAccountBalance(): boolean {
+    //stub method
+    return false;
+  }
+
+  /**
+   * setBalance attempts to set the balance to the argument passed to the function.
+   * @param balance the balnce we wish to set into our service.
+   * @return a boolean indicating if the set was succesful.
+   */
+  public setBalance(balance: number): boolean {
+    return false;
+  }
+
+  /**getBalance() returns the balance value stored in this service.
+   * @return number returns a number if we have declared a balance.
+   * @return null returns null if no balance has been set
+   */
+  public getBalance(): number | null {
+    return null;
+  }
 }
