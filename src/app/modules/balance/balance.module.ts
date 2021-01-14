a37f29451a8a87ed8b8a7183698a1e27543e7b62
Hieu Le <htl43@drexel.edu>
2021-01-14 11:54:31 -0500

    initial for balance component

---
+++ src/app/modules/balance/balance.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BalanceComponent } from './component/balance/balance.component';
+
+
+
+@NgModule({
+  declarations: [BalanceComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BalanceModule { }
