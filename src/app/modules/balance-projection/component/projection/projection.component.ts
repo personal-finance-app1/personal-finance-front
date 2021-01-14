72c9d3a35596c4737db6602e1d1de1021d21770f
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-14 09:24:45 -0600

    Added empty service and component to feature module

---
+++ src/app/modules/balance-projection/component/projection/projection.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projection',
+  templateUrl: './projection.component.html',
+  styleUrls: ['./projection.component.css']
+})
+export class ProjectionComponent implements OnInit {
+
+  account:any = {income: 1200, expenses: 800, balanace: 1800};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
