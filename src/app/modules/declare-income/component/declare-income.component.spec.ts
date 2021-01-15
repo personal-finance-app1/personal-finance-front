3ef87d1a7a9a9a87426c615290fbbb8729b0a090
magnusjw <magnusjw@clarkson.edu>
2021-01-15 14:15:04 -0500

    First draft of front end tests

--- src/app/modules/declare-income/component/declare-income.component.spec.ts
+++ src/app/modules/declare-income/component/declare-income.component.spec.ts
@@ -31,21 +31,35 @@ describe('DeclareIncomeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should call the sendIncome method on the declare-incomeService', () => {
-  //   spy = spyOn(service, 'sendIncome');
-  //   component.onSubmit();
-  //   expect(spy).toHaveBeenCalled();
-  // });
+   
 
   describe('onSubmit method', () => {
+    it('should call the sendIncome method in the declare-incomeService', () => { // Successful
+      spy = spyOn(service, 'sendIncome');
+      component.onSubmit();
+      expect(spy).toHaveBeenCalled();
+    });
+    
     xit('should have userInput', () => { // Successful
       //component.userInput = 15;
       expect(component.userInput).toBeTruthy();
     });
+
     xit('if userInput is null', () => { // Negative
       //component.userInput = null;
+      expect(component.userInput).toBeFalsy();
+    });
+
+    xit('if userInput is negative', () => { // Negative
+      expect(component.userInput).toBeLessThan(0);
+    });
+
+    xit('if userInput is incorrectly using the decimal', () => { // Negative
       expect(component.userInput).toBeFalsy();
     });
   });
 
 });
+
+// $150.005
+// $150.01
--- src/app/modules/declare-income/service/declare-income.service.spec.ts
+++ src/app/modules/declare-income/service/declare-income.service.spec.ts
@@ -5,11 +5,14 @@ import { DeclareIncomeService } from './declare-income.service';
 describe('DeclareIncomeService', () => {
   let service: DeclareIncomeService;
 
+  let returnValue: number;
+  let account: Account;
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(DeclareIncomeService);
 
-    service.sendIncome(15);
+     //httpSpy
 
   });
 
@@ -18,15 +21,58 @@ describe('DeclareIncomeService', () => {
   });
 
   describe('sendIncome method', () => {
-    xit('should have userInput', () => { // Successful
-      //component.userInput = 15;
-      expect().toBeTruthy(); //httpSpy
+
+    it('should have userInput', () => { // Successful
+      expect(service.account).toBeTruthy();
     });
-    xit('if userInput is null', () => { // Negative
-      //component.userInput = null;
-      expect().toBeFalsy();
+
+    it('should return positive status code', () => { // Successful
+      account = new Account(500, 300, 500);
+      returnValue = service.sendIncome(account); // leverage httpSpy
+      expect(returnValue).toBeLessThan(300);
     });
+
+    xit('should return a negative status code', () => { // Negative
+      account = new Account(-500, -300, -500);
+      returnValue = service.sendIncome(account); // leverage httpSpy
+      expect(returnValue).toBeGreaterThanOrEqual(400);
+    });
+
+    xit('should be given the correct parameters', () => { // Negative
+
+      returnValue = service.sendIncome(account); // leverage httpSpy
+      expect(returnValue).toBeFalsy();
+    });
+
+    //accomodate for an account that is passed with no validation,
+    //such as sendIncome being called from a different method
+
+
   });
-
-
 });
+
+// describe('onSubmit method', () => {
+//   it('should call the sendIncome method in the declare-incomeService', () => { // Successful
+//     spy = spyOn(service, 'sendIncome');
+//     component.onSubmit();
+//     expect(spy).toHaveBeenCalled();
+//   });
+  
+//   xit('should have userInput', () => { // Successful
+//     //component.userInput = 15;
+//     expect(component.userInput).toBeTruthy();
+//   });
+
+//   xit('if userInput is null', () => { // Negative
+//     //component.userInput = null;
+//     expect(component.userInput).toBeFalsy();
+//   });
+
+//   xit('if userInput is negative', () => { // Negative
+//     expect(component.userInput).toBeLessThan(0);
+//   });
+
+//   xit('if userInput is incorrectly using the decimal', () => { // Negative
+//     expect(component.userInput).toBeFalsy();
+//   });
+// });
