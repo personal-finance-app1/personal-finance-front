9c7e60c3e045e009651ed13213b26b58108fa664
Hieu Le <htl43@drexel.edu>
2021-01-15 09:55:27 -0500

    adding template for balance and material

--- src/app/modules/balance/service/balance.service.ts
+++ src/app/modules/balance/service/balance.service.ts
@@ -8,7 +8,7 @@ export class BalanceService {
   constructor() { }
 
   /**The validate account balance is used to ensure the user has entered a valid declaring or update balance.
-  * @returns boolean which indicates if the balance is valid or invalid.
+  * returns boolean which indicates if the balance is valid or invalid.
   */
   public validateAccountBalance(): boolean {
     //stub method
@@ -17,16 +17,16 @@ export class BalanceService {
 
   /**
    * setBalance attempts to set the balance to the argument passed to the function.
-   * @param balance the balnce we wish to set into our service.
-   * @return a boolean indicating if the set was succesful.
+   * param balance the balnce we wish to set into our service.
+   * return a boolean indicating if the set was succesful.
    */
   public setBalance(balance: number): boolean {
     return false;
   }
 
   /**getBalance() returns the balance value stored in this service.
-   * @return number returns a number if we have declared a balance.
-   * @return null returns null if no balance has been set
+   * return number returns a number if we have declared a balance.
+   * return null returns null if no balance has been set
    */
   public getBalance(): number | null {
     return null;
