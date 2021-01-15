4c47ba0f792d525c47963335578407e7b77d3bc9
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-15 16:32:54 -0600

    Updated module to include charts and mat slider

--- src/app/modules/balance-projection/balance-projection.module.ts
+++ src/app/modules/balance-projection/balance-projection.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectionComponent } from './component/projection/projection.component';
+import { MatSliderModule  } from '@angular/material/slider';
 import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [ProjectionComponent],
-  imports: [CommonModule, ChartsModule],
+  imports: [CommonModule, MatSliderModule, ChartsModule],
   exports: [ProjectionComponent]
 })
 export class BalanceProjectionModule { }
