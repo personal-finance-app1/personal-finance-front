02197726ef3016948256fda86b0c4c418af86954
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-15 10:28:59 -0600

    Created an Account model and added a Subject<Account> to the environment to act as our dynamic account object.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+import { Subject } from "rxjs";
+import { Account } from "src/app/models/account";
+
 export const environment = {
   production: true
 };
+
+export let account$: Subject<Account> = new Subject<Account>();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 import { Subject } from "rxjs";
+import { Account } from "src/app/models/account";
 
 export const environment = {
   production: false
 };
 
-export let account: Subject<any>;
+export let account$: Subject<Account> = new Subject<Account>();
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
