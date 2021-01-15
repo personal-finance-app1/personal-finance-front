a38a52e1ed25f340002087830996ef10b19fb472
logriffith <logriffith1991@gmail.com>
2021-01-15 12:50:42 -0700

    changed environment import

--- src/app/modules/balance-projection/component/projection/projection.component.ts
+++ src/app/modules/balance-projection/component/projection/projection.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Account } from 'src/environments/environment';
+import { account$ } from 'src/environments/environment';
 
 @Component({
   selector: 'app-projection',
