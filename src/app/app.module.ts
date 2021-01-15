bb22a98c0dafbdb439948d3d140a4e8ba6b517a0
magnusjw <magnusjw@clarkson.edu>
2021-01-15 15:16:57 -0500

    merging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BalanceProjectionModule } from './modules/balance-projection/balance-projection.module';
+import { DeclareBalanceComponent } from './declareBalance/declare-balance/declare-balance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeclareBalanceComponent
   ],
   imports: [
     BrowserModule,
