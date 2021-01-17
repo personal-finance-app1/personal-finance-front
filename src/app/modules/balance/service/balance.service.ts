715fbbc671e95c162dc22b2798edee339156b99b
Berin <berin2@pdx.edu>
2021-01-16 19:02:53 -0800

    Rebasing with dev.
    I am rebasing our branch with dev.

--- src/app/modules/balance/service/balance.service.ts
+++ src/app/modules/balance/service/balance.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BalanceService {
+  constructor() {}
 
-  constructor() { }
+  //eventually going to take out for global service field
+  accountBalance: number;
 
   /**The validate account balance is used to ensure the user has entered a valid declaring or update balance.
-  * returns boolean which indicates if the balance is valid or invalid.
-  */
-  public validateAccountBalance(): boolean {
-    //stub method
+   * returns boolean which indicates if the balance is valid or invalid.
+   */
+  public validateAccountBalance(accountBalance: number): boolean {
     return false;
   }
 
   /**
    * setBalance attempts to set the balance to the argument passed to the function.
-   * param balance the balnce we wish to set into our service.
-   * return a boolean indicating if the set was succesful.
+   * @param balance the balance we wish to set into our service.
+   * @return a boolean indicating if the set was succesful.
    */
   public setBalance(balance: number): boolean {
     return false;
