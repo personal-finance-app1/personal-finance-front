44698ddd44f9eeea32bc25188bc690dc355aa474
Grant Wiswell <wiswellgrant@gmail.com>
2021-01-19 14:10:39 -0600

    Got the line chart up and running

--- src/app/modules/balance-projection/balance-projection.module.ts
+++ src/app/modules/balance-projection/balance-projection.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectionComponent } from './component/projection/projection.component';
 import { MatSliderModule  } from '@angular/material/slider';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [ProjectionComponent],
-  imports: [CommonModule, MatSliderModule, ChartsModule],
+  imports: [CommonModule, MatSliderModule, MatGridListModule, ChartsModule],
   exports: [ProjectionComponent]
 })
 export class BalanceProjectionModule { }
