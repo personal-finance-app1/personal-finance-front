2b24eb0fbbbe458c8f120f84fc9c4e93e2929254
akhilapalaparthi <73906963+akhilapalaparthi@users.noreply.github.com>
2021-01-20 14:19:08 -0600

    Declare expenses (#6)
    * initial commit
    
    * Initial Angular Setup
    
    * file hierarchy
    
    * setup the html
    
    * create empty methods
    
    * rebase dev to main
    
    * Rebase dev with main
    
    * Initial Angular Setup
    
    * changes made
    
    * added spy to testing components
    
    * Added account subject to environment.ts
    
    * Created an Account model and added a Subject<Account> to the environment to act as our dynamic account object.
    
    * preparing to write unit tests
    
    * file hierarchy
    
    * setup the html
    
    * create empty methods
    
    * changes made
    
    * added spy to testing components
    
    * preparing to write unit tests
    
    * added two tests for updateExpenses()
    
    * added decimal test for updateExpenses()
    
    * minor fixes
    
    * testing unit tests
    
    * small fixes
    
    * Updated testing
    
    * created unit test for declare-expenses service
    
    * Delete .browserslistrc
    
    * Delete .editorconfig
    
    * Delete .gitignore
    
    * Delete README.md
    
    * Delete angular.json
    
    * Delete debug.log
    
    * Delete protractor.conf.js
    
    * Delete app.e2e-spec.ts
    
    * Delete app.po.ts
    
    * Delete tsconfig.json
    
    * Delete karma.conf.js
    
    * Delete package-lock.json
    
    * fixing PR errors
    
    Co-authored-by: Grant Wiswell <wiswellgrant@gmail.com>
    Co-authored-by: askakhila <askakhila@yahoo.com>
    Co-authored-by: cwsvensson <cws.commercial@gmail.com>
    Co-authored-by: topheryun <46732742+topheryun@users.noreply.github.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DeclareExpensesModule } from './modules/declare-expenses/declare-expenses.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,14 +15,17 @@ import { AuthModule } from './modules/auth/auth.module';
 @NgModule({
   declarations: [
     AppComponent,
-    DeclareIncomeComponent,
-    HeaderComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    DeclareExpensesModule,
+    DeclareIncomeComponent,
+    HeaderComponent,
+    LoginComponent,
     BalanceProjectionModule,
     AuthModule
   ],
