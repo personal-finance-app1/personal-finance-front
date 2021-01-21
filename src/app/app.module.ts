9d90bf5e955a00ee2f698938f0df3043b19680e9
MukeshChaudhary <mukesh.chaudhary@revature.net>
2021-01-21 10:28:36 -0600

    sovled conflict file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
+
 import { BalanceProjectionModule } from './modules/balance-projection/balance-projection.module';
-=======
+
 import { HeaderComponent } from './modules/navigation/header/header.component';
 import { LoginComponent } from './modules/auth/components/login/login.component';
 import { AuthModule } from './modules/auth/auth.module';
 
-
-
->>>>>>> auth
 
 @NgModule({
   declarations: [
@@ -24,12 +21,7 @@ import { AuthModule } from './modules/auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
     BalanceProjectionModule
-=======
-    AuthModule
-
->>>>>>> auth
   ],
   providers: [],
   bootstrap: [AppComponent]
