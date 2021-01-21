9f816e04664d4ff1066f5941b483e2a92588c972
magnusjw <magnusjw@clarkson.edu>
2021-01-21 14:12:40 -0500

    Validation for negative and poor decimal values

--- src/app/modules/declare-income/component/declare-income.component.ts
+++ src/app/modules/declare-income/component/declare-income.component.ts
@@ -26,16 +26,20 @@ export class DeclareIncomeComponent implements OnInit {
 
   //We want to display the environment account.income as the current income in the widget
 
-  //Math.trunc()
-
   public onSubmit(){
 
+    console.log("User Input is: " + this.userInput)
+
     if(this.userInput < 0){
+      
       this.account.income = 0; //reset input to 0
       this.error = "Error: Input must be positive.";
+
     } else if(Math.floor(this.userInput * 100) != (this.userInput * 100)){
+      
       this.account.income = 0; //reset input to 0
       this.error = "Error: Input cannot exceed two decimal places.";
+
     } else {
       this.account.income = this.userInput; //Set income of localAccount
       this.ds.sendIncome(this.account).subscribe((response: Account)  => { //More Questions Here
