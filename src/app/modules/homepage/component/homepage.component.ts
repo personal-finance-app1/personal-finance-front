ed4925ab998109db7dbd9c5bddeb2e67e7121cd9
logriffith <logriffith1991@gmail.com>
2021-01-25 09:57:55 -0700

    working on homepage format

--- src/app/modules/homepage/component/homepage.component.ts
+++ src/app/modules/homepage/component/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from 'src/app/models/account';
+import { account$ } from 'src/environments/environment';
 
 export interface Tile {
   color: string;
@@ -15,16 +17,24 @@ export interface Tile {
 
 export class HomepageComponent implements OnInit {
 
+  account : Account;
+
   constructor() { }
 
   ngOnInit(): void {
+    //get the account from the environment variable account to get account info
+    account$.subscribe(
+      (account) => {
+        this.account = account;
+      }
+    );
   }
 
-  tiles: Tile[] = [
-    {text: 'Income', cols: 5, rows: 3, color: '#FFFFFF'},
-    {text: 'Balance Projection', cols: 7, rows: 4, color: '#FFFFFF'},
-    {text: 'Expenses', cols: 5, rows: 3, color: '#FFFFFF'},
-    {text: 'Update Balance', cols: 7, rows: 2, color: '#FFFFFF'},
-  ];
+  // tiles: Tile[] = [
+  //   {text: 'Income', cols: 5, rows: 3, color: '#FFFFFF'},
+  //   {text: 'Balance Projection', cols: 7, rows: 4, color: '#FFFFFF'},
+  //   {text: 'Expenses', cols: 5, rows: 3, color: '#FFFFFF'},
+  //   {text: 'Update Balance', cols: 7, rows: 2, color: '#FFFFFF'},
+  // ];
 
 }
